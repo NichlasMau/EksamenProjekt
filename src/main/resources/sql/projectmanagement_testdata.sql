b68d28a9e3bc514b23eb3b0b5c21cbd551dbb520
Samim <sami0880@stud.kea.dk>
2023-05-08 10:40:32 +0200

    SQL in resources

---
+++ src/main/resources/sql/projectmanagement_testdata.sql
@@ -0,0 +1,34 @@
+INSERT INTO users (name, email, username, password) VALUES
+('John Doe', 'johndoe@example.com', 'johndoe', 'password123'),
+('Jane Smith', 'janesmith@example.com', 'janesmith', 'password456'),
+('Bob Johnson', 'bobjohnson@example.com', 'bobjohnson', 'password789');
+
+INSERT INTO projects (name, description, status, budget, start_date, end_date) VALUES
+('Website Redesign', 'Redesigning company website for better user experience', 'doing', 5000.00, '2023-06-01 00:00:00', '2023-08-31 00:00:00'),
+('Mobile App Development', 'Developing a mobile app for iOS and Android platforms', 'todo', 10000.00, '2023-07-01 00:00:00', '2023-10-31 00:00:00'),
+('Social Media Campaign', 'Creating a social media campaign to increase brand awareness', 'done', 2500.00, '2023-05-01 00:00:00', '2023-06-30 00:00:00');
+
+INSERT INTO subprojects (project_id, name, description, status, budget, start_date, end_date) VALUES
+(1, 'Homepage Redesign', 'Redesigning the homepage to make it more user-friendly', 'doing', 2000.00, '2023-06-01 00:00:00', '2023-06-30 00:00:00'),
+(1, 'Product Page Redesign', 'Redesigning the product page to improve conversions', 'todo', 3000.00, '2023-07-01 00:00:00', '2023-07-31 00:00:00'),
+(2, 'iOS Development', 'Developing the iOS version of the mobile app', 'todo', 5000.00, '2023-07-01 00:00:00', '2023-08-31 00:00:00');
+
+INSERT INTO tasks (subproject_id, name, description, status, start_date, end_date, budget, estimated_time) VALUES
+(1, 'Design Wireframes', 'Create wireframes for the new homepage design', 'doing', '2023-06-01 00:00:00', '2023-06-07 00:00:00', 500.00, 20.0),
+(1, 'Design Mockups', 'Create mockups based on wireframes for homepage redesign', 'todo', '2023-06-08 00:00:00', '2023-06-14 00:00:00', 750.00, 30.0),
+(2, 'Develop Backend', 'Develop backend functionality for the mobile app', 'todo', '2023-07-01 00:00:00', '2023-07-31 00:00:00', 2000.00, 80.0);
+
+INSERT INTO project_members (user_id, hourly_rate, project_id) VALUES
+(1, 50.00, 1),
+(2, 75.00, 1),
+(3, 100.00, 2);
+
+INSERT INTO customers (name, email, phone) VALUES
+('ABC Corporation', 'abccorp@example.com', '123-456-7890'),
+('XYZ LLC', 'xyzllc@example.com', '555-555-5555'),
+('Acme Inc', 'acmeinc@example.com', NULL);
+
+INSERT INTO project_customers (customer_id, project_id) VALUES
+(1, 1),
+(2, 2),
+(3, 3);
---
+++ src/main/resources/sql/projectmanagementdb.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE IF NOT EXISTS projectmanagementDb;
+USE projectmanagementDb;
+
+CREATE TABLE users (
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  email varchar(255) NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE projects (
+  project_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  description TEXT,
+  status ENUM('todo', 'doing', 'done') NOT NULL DEFAULT 'todo',
+  budget FLOAT NOT NULL,
+  start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  end_date TIMESTAMP
+);
+
+CREATE TABLE subprojects (
+  subproject_id INT PRIMARY KEY AUTO_INCREMENT,
+  project_id INT NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  description TEXT,
+  status ENUM('todo', 'doing', 'done') NOT NULL DEFAULT 'todo',
+  budget FLOAT NOT NULL,
+  start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  end_date TIMESTAMP,
+  FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE
+);
+
+CREATE TABLE tasks (
+  task_id INT PRIMARY KEY AUTO_INCREMENT,
+  subproject_id INT NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  description TEXT,
+  status ENUM('todo', 'doing', 'done') NOT NULL DEFAULT 'todo',
+  start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  end_date TIMESTAMP,
+  budget FLOAT NOT NULL,
+  estimated_time FLOAT NOT NULL,
+  FOREIGN KEY (subproject_id) REFERENCES subprojects(subproject_id) ON DELETE CASCADE
+);
+
+CREATE TABLE project_members (
+  project_member_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  hourly_rate FLOAT NOT NULL,
+  project_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE
+);
+
+CREATE TABLE customers (
+  customer_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone VARCHAR(20)
+);
+
+CREATE TABLE project_customers (
+  project_customer_id INT PRIMARY KEY AUTO_INCREMENT,
+  customer_id INT NOT NULL,
+  project_id INT NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE,
+  FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE
+);
